eb496731d102747388fb4750c4f3187fdb095130
xiangdefei <xiangdefei@we.com>
2017-12-07 14:24:46 +0800

    Code Push Test 项目初始化

---
+++ FECodePushTest/FECodePushTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FECodePushTest
+//
+//  Created by FlyElephant on 2017/12/6.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FECodePushTest/FECodePushTest/ViewController.m
@@ -0,0 +1,102 @@
+//
+//  ViewController.m
+//  FECodePushTest
+//
+//  Created by FlyElephant on 2017/12/6.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <React/RCTRootView.h>
+#import "CodePush.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self setup];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setup {
+    NSURL *jsCodeLocation = [CodePush bundleURL];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"FECodePushTest"
+                         initialProperties :
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"FlyElephant",
+                   @"value": @"100"
+                   },
+               @{
+                   @"name" : @"Keso",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                          launchOptions    : nil];
+    rootView.frame = CGRectMake(0, 150, [UIScreen mainScreen].bounds.size.width, 300);
+    [self.view addSubview:rootView];
+}
+
+- (void)setup2 {
+    NSURL *jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"bundle/index.ios" withExtension:@"jsbundle"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"FECodePushTest"
+                         initialProperties :
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"FlyElephant",
+                   @"value": @"100"
+                   },
+               @{
+                   @"name" : @"Keso",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                          launchOptions    : nil];
+    rootView.frame = CGRectMake(0, 150, [UIScreen mainScreen].bounds.size.width, 300);
+    [self.view addSubview:rootView];
+}
+
+- (void)setup1 {
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://127.0.0.1:8081/index.ios.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"FECodePushTest"
+                         initialProperties :
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"FlyElephant",
+                   @"value": @"100"
+                   },
+               @{
+                   @"name" : @"Keso",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                          launchOptions    : nil];
+    rootView.frame = CGRectMake(0, 150, [UIScreen mainScreen].bounds.size.width, 300);
+    [self.view addSubview:rootView];
+}
+
+
+@end
